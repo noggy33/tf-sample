175c997f37f4652ae48b1f854228a1b335a097b3
Nogiwa, Kensaku <kensaku.nogiwa@gmail.com>
2022-06-08 18:54:03 +0900

    test

--- main.tf
+++ main.tf
@@ -71,11 +71,11 @@ resource ibm_is_instance "vsi1" {
   }
 }
 
-resource ibm_is_floating_ip "fip1" {
-  count = 0
-  name = "${local.BASENAME}-fip1"
-  target = ibm_is_instance.vsi1.primary_network_interface.0.id
-}
+#resource ibm_is_floating_ip "fip1" {
+#  count = 0
+#  name = "${local.BASENAME}-fip1"
+#  target = ibm_is_instance.vsi1.primary_network_interface.0.id
+#}
 
 #output sshcommand {
 #  value = "ssh root@${ibm_is_floating_ip.fip1.address}"
