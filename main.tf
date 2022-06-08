50cb9f1388856a09e26e196437eac3b684c239d4
Nogiwa, Kensaku <kensaku.nogiwa@gmail.com>
2022-06-08 10:38:08 +0900

    test

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "ibm_is_instances" "example" {
 
 locals {
   xyz = length(data.ibm_is_instances.example.instances) >= 1 ? true : false
-  abc = xyz ? data.ibm_is_instances.example.instances.1 : null
+  abc = xyz ? data.ibm_is_instances.example.instances.0 : null
 }
 
 output "instance_count" {
