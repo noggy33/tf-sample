8a996dd77696a8c57849b9a49b46ea10fcef1f49
Nogiwa, Kensaku <kensaku.nogiwa@gmail.com>
2022-06-08 15:22:58 +0900

    test

--- main.tf
+++ main.tf
@@ -25,6 +25,24 @@ resource ibm_is_security_group "sg1" {
   vpc = ibm_is_vpc.vpc.id
 }
 
+resource "ibm_is_security_group_rule" "ingress_ssh_all" {
+  group = ibm_is_security_group.sg1.id
+  direction = "inbound"
+  remote = "0.0.0.0/0"
+
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource ibm_is_subnet "subnet1" {
+  name = "${local.BASENAME}-subnet1"
+  vpc = ibm_is_vpc.vpc.id
+  zone = "${local.ZONE}"
+  total_ipv4_address_count = 256
+}
+
 #data "ibm_is_instances" "example" {
 #}
 
