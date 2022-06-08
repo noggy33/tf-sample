06bde364666f02223a9870f447813b21b69f2112
Nogiwa, Kensaku <kensaku.nogiwa@gmail.com>
2022-06-08 18:33:29 +0900

    test

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ data "ibm_is_instances" "example" {
 locals {
   xyz = length(data.ibm_is_instances.example.instances) >= 1 ? true : false
   abc = local.xyz ? data.ibm_is_instances.example.instances.0.id : null
-  mode_default = "on"
+  mode_default = "hoge"
   mode = local.abc != null ? local.abc == "on" ? "off" : "on" : local.mode_default
 }
 
