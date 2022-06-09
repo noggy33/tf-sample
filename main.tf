05914e6735593a762dae00da789d9a44cf5f90b9
Nogiwa, Kensaku <kensaku.nogiwa@gmail.com>
2022-06-09 14:44:30 +0900

    test

--- main.tf
+++ main.tf
@@ -65,30 +65,33 @@ locals {
   # 既存のインスタンスが存在するか確認する。
   is_target = length(local.target) >= 1 ? true : false
 
-  # 既存インスタンスがあれば、statusを取得する。無ければ、"null"を設定する。
+  # 既存インスタンスがあれば、"tags"を取得する。無ければ、"null"を設定する。
   # - "name"と合致するインスタンスは、1つしかない前提
-  status = local.is_target ? local.target.0.status : null
+  tag_current = local.is_target ? local.target.0.tags : null
 
-  # 既存インスタンスの状態が"running"であれば止める。"running"で無ければ起動する。既存インスタンスがない場合は起動する。
-  # 既存インスタンスが存在しない場合の挙動が定まらないので、"is_target"が"false"の場合は、"action_default"を設定している。
-  tag_cf1 = {
-    tags = ["cf1"]
-  }
-
-  tag_cf2 = {
-    tags = ["cf2"]
-  }
-
+  # 既存インスタンスの"tags"が"on"であれば"off"に変える。
+  #                           "on"で無ければ"on"にする。
+  # 既存インスタンスがない場合はDefault値にする。
   tag_default = {
-    tags = ["initial"]
+    tags = ["on"]
   }
 
-  tag_next = local.is_target == true ? local.status == "running" ? local.tag_cf1 : local.tag_cf2 : local.tag_default
-#   mode_next = local.mode == local.mode_initial ? local.mode == "on" ? ["off"] : ["on"] : []
-#   mode_list = concat(mode_next, mode_default)
-  empty = []
-  numbers = ["one", "two", "three"]
-  test_list = concat(local.empty, local.numbers)
+  tag_on = {
+    tags = ["on"]
+  }
+
+  tag_off = {
+    tags = ["off"]
+  }
+
+  # 現在のタグと設定したいタグが等しい場合、かつ、
+  #   - 現在のタグが"on"の場合: "tag_next"を"off"にする。
+　#   - 現在のタグが"on"でない場合: "tag_next"を"on"にする。
+  # 現在のタグと設定したいタグが等しくない場合、
+  #   - "tag_next"は、空にする。
+  tag_next = local.tag_current == local.tag_default.tags.0 ? local.tag_current == "on" ? local.tag_off.tags : local.tag_on.tags : []
+  
+  tag_result = concat(local.tag_next, local.tag_default.tags)
 }
 
 resource ibm_is_instance "vsi1" {
@@ -99,7 +102,7 @@ resource ibm_is_instance "vsi1" {
   keys = [data.ibm_is_ssh_key.ssh_key_id.id]
   image = data.ibm_is_image.ubuntu.id
   profile = "bx2-2x8"
-  tags = local.tag_next.tags
+  # tags = local.tag_next.tags
 
   primary_network_interface {
     subnet = ibm_is_subnet.subnet1.id
@@ -118,7 +121,7 @@ output vpc_id {
 output "instance_count" {
 #  description = "Number of instances"
   #value = local.tag_next
-  value = local.test_list
+  value = local.tag_result
 #  value = data.ibm_is_instances.example.instances.0.id
 }
 
